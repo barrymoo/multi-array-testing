7f6de4fa7cfb1eca550ba5b2e527e68a1bd2218d
barrymoo <moore0557@gmail.com>
2016-03-11 23:03:42 -0500

    Add ma cublas example

---
+++ ma-blas.cpp
@@ -0,0 +1,29 @@
+#include <boost/multi_array.hpp>
+#include <omp.h>
+#include <iostream>
+#include "cblas.h"
+#include "cblas_f77.h"
+
+int main () {
+  typedef boost::multi_array<double, 1> vector;
+  typedef vector::index vector_index;
+
+  int N = 100000000;
+
+  vector x(boost::extents[N]);
+  vector y(boost::extents[N]);
+  vector a(boost::extents[N]);
+
+#pragma omp parallel for
+  for (vector_index i = 0; i < N; i++) {
+    x[i] = 1;
+    y[i] = 1;
+    a[i] = y[i];
+  }
+
+  cblas_daxpy(N, 1.0, &x[0], 1, &a[0], 1);
+
+  std::cout << a[0] << '\n';
+
+  return 0;
+}
